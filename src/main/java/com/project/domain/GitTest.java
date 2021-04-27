67dea6db604808cafdbfc20c022e4ee9018870a5
Mrdong <3026745569@qq.com>
2021-04-27 18:27:33 +0800

    shop commit 1.1

--- src/main/java/com/project/domain/GitTest.java
+++ src/main/java/com/project/domain/GitTest.java
@@ -3,5 +3,6 @@ package com.project.domain;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 1");
     }
 }
