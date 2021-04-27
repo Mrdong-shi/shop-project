3906c108697cc4957f9052917dc4c7e049c3ebdd
Mrdong-shi <83216379+Mrdong-shi@users.noreply.github.com>
2021-04-27 19:38:37 +0800

    Update GitTest.java

--- src/main/java/com/project/domain/GitTest.java
+++ src/main/java/com/project/domain/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git master");
         System.out.println("hello git hotfix-tx");
         System.out.println("1111111111111111111111111111111push test");
+        System.out.println("2222222222212222222222222222222push test");
     }
 }
